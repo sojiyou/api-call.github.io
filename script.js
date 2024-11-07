e818560caa487d6e459ee48b220deb40e2dacff5
Sojo Decaran <sojodecaran200@gmail.com>
2024-11-07 08:42:17 +0800

    fixed the fetchData function to fetch API

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 async function fetchData(){
+      try {
+         const response = await fetch('https://bible-api.com/john 3:16?translation=kjv');
+         if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+         }
+         const data = await response.json();
+         const verseData = data.verses[0];
 
-    try{
-        const response = await fetch(`https://bible-api.com/john 3:16`);
-        if(!response.ok){
-            throw new Error("404 not found");
-        }
-    
-    }catch(error){
-        console.error(error);
-    }
-    
-    }
-    
-    fetchData();
+         const bibleBook = verseData.book_name;
+         const bibleChapter = verseData.chapter;
+         const bibleVerse = verseData.verse;
+         const bibleText = verseData.text;
+
+         console.log(bibleBook);
+         console.log(bibleChapter);
+         console.log(bibleVerse);
+         console.log(bibleText);
+      } catch (error) {
+         console.error('Error:', error);
+      }
+}
+
+fetchData();
+
+
+
